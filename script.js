79a6cf3bac8b22a64a15d4fe989e6cfe2edb82b9
colin11702 <colinmcherney@gmail.com>
2023-07-14 11:42:36 -0700

    Changed grid setup and added slider
    Setting up grid and slider so that there can be more or less divs

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-function gridSetup() {
+let divs = 0;
+
+function gridSetup(divCount) {
   let counter = 0;
-  while (counter < 256) {
+  divs = divCount * divCount;
+  while (counter < divs) {
     const div = document.createElement("div");
     div.className = "grid";
+    div.style.backgroundColor = "white";
+    let height = 400 / divCount + "px";
+    let width = 400 / divCount + "px";
+    div.style.height = height;
+    div.style.width = width;
     document.getElementById("container").appendChild(div);
     counter++;
   }
+  return divs;
 }
 
-gridSetup();
+gridSetup(16);
 
 const grid = document.getElementsByClassName("grid");
 
 function customColor(color) {
   color = document.getElementById("colorCode").value;
-  for (i = 0; i < 256; i++) {
+  for (i = 0; i < divs; i++) {
     grid[i].onmouseover = function () {
       this.style.backgroundColor = color;
     };
@@ -22,7 +31,7 @@ function customColor(color) {
 }
 
 function color(color) {
-  for (i = 0; i < 256; i++) {
+  for (i = 0; i < divs; i++) {
     grid[i].onmouseover = function () {
       this.style.backgroundColor = color;
     };
@@ -30,7 +39,7 @@ function color(color) {
 }
 
 function reset() {
-  for (i = 0; i < 256; i++) {
+  for (i = 0; i < divs; i++) {
     grid[i].style.backgroundColor = "white";
   }
 }
